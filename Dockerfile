4a4d38ffee9a15c1acd35333395f23ceb747f0df
c0decafe <220505+c0decafe@users.noreply.github.com>
2023-06-13 17:07:32 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM cloudron/base:4.0.0@sha256:31b195ed0662bdb06a6e8a5ddbedb6f191ce92e8bee04c03fb02dd4e9d0286df
+
+ARG VERSION=4.13.0
+ARG DEB=code-server_${VERSION}_amd64.deb
+ARG URL=https://github.com/coder/code-server/releases/download/v${VERSION}/${DEB}
+ARG USER=cloudron
+ARG HOME=/home/${USER}
+
+RUN curl -L -O ${URL} && apt install ./${DEB} && rm ${DEB}
+
+COPY config.yaml ${HOME}/.config/code-server/config.yaml
+COPY start.sh /app/pkg/start.sh
+
+RUN ln -s /run/.cache/ ${HOME}/.cache \
+    && mkdir ${HOME}/workspace \
+    && ln -s /app/data/workspace/ ${HOME}/workspace \
+    && chown -R ${USER}:${USER} ${HOME}
+
+CMD [ "/app/pkg/start.sh" ]
---
+++ start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu
+
+OWN="cloudron:cloudron"
+GSU="/usr/local/bin/gosu $OWN"
+ENV="/app/data/env"
+
+if [[ ! -f $ENV  ]]; then
+     $GSU cat > $ENV <<EOF
+CS_DISABLE_GETTING_STARTED_OVERRIDE=false
+EOF
+fi
+
+source $ENV
+
+export XDG_DATA_HOME=/app/data/.local/share
+export XDG_STATE_HOME=/app/data/.local/state
+export XDG_RUNTIME_DIR=/run
+export XDG_CACHE_HOME=/run/.cache
+
+echo "==> Ensure permissions"
+chown -R $OWN /run/.cache /app/data
+
+if [[ ! -f /app/data/.initialized  ]]; then
+     echo "Fresh installation, init"
+     $GSU code-server --install-extension Gitlab.gitlab-workflow
+     touch /app/data/.initialized
+fi
+
+if [[ -d /app/data/.vscode/logs  ]]; then
+      $GSU mv /app/data/.vscode/logs /run
+      $GSU ln -s /run/logs /app/data/.vscode/logs
+fi
+
+echo "==> Starting code-server"
+exec $GSU code-server
